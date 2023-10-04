f5d88cde9ddce1c678b17954cfea51246b1cfba7
Ajay <ajay.naik44@gmail.com>
2023-10-04 23:09:48 +0000

    PE01-Ajay Submission

---
+++ PE01/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     
+      <Text style={styles.text}>Ajay Naik</Text>
+      <Text style={styles.text}>MSCS</Text>
+      <Text style={styles.text}>STC</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
