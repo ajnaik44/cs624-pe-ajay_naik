45978cf43179cfccb0da99139281659cdb371615
Ajay <ajay.naik44@gmail.com>
2023-10-27 01:44:12 +0000

    PE03 SUbmission

--- PE03-todos/App.js
+++ PE03-todos/App.js
@@ -1,77 +1,73 @@
-// Listing 3.8 Creating the inputChange function
-
-/*
-  Write a function to get the value of the TextInput and save it to the state of the App component. 
-  inputChange={(text) => this.inputChange(text)}
-
-  Add a new function called inputChange below the constructor and above the render function. 
-  This function will update the state value of inputValue with the value passed in.
-  
-  Log out the value of inputValue for you to make sure the function is working by using console.log().
-*/
-import React, { Component } from 'react'
-import { View, ScrollView, StyleSheet } from 'react-native'
-
-import Heading from './Heading'
-import Input from './Input'
+import React, { Component } from "react";
+import { View, ScrollView, StyleSheet, Button } from 'react-native'; 
+import Heading from './Heading';
+import Input from './Input';
+import TodoItem from './TodoItem'; 
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       inputValue: '',
       todos: [],
-      type: 'All'
+      type: 'All',
+    };
+  }
+
+  inputChange(inputValue) {
+    console.log('Input Value: ', inputValue);
+    this.setState({ inputValue });
+  }
+
+  // Create a function to handle adding new to-do items
+  addTodo() {
+    if (this.state.inputValue !== '') {
+      const newTodos = this.state.todos.slice();
+      newTodos.push(this.state.inputValue);
+      this.setState({
+        todos: newTodos,
+        inputValue: '',
+      });
+      console.log('New Todo Added:', this.state.inputValue);
     }
   }
 
-  /*
-    Creates the inputChange method, which takes inputValue as an argument.
-
-    Logs out the inputValue value to make sure the method is working.
-
-    setState() 
-      https://reactjs.org/docs/react-component.html#setstate
-      It enqueues changes to the component state and tells React that this component and its children need to be re-rendered with the updated state.
-
-    Sets the state with the new value—same as this.setState({inputValue: inputValue}).
-  */
-  inputChange(inputValue) {
-    console.log(' Input Value: ' , inputValue)
-    this.setState({ inputValue })
-  }
-
-  /*
-    <Input >
-    Passes inputValue as a property to the Input component.
-    Passes inputChange as a property to the Input component.
-  */
   render() {
+    const { inputValue, todos } = this.state;
+
     return (
       <View style={styles.container}>
-        <ScrollView keyboardShouldPersistTaps='always'
-                    style={styles.content}>
-          <View>
-            <Heading />
-            <Input 
-              inputValue={this.state.inputValue}
-              inputChange={(text) => this.inputChange(text)} />
-          </View>
+        <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={text => this.inputChange(text)}
+          />
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <Button
+            title="Submit"
+            onPress={() => this.addTodo()}
+            style={{ backgroundColor: 'white' }}
+          />
+        </View>
+          {todos.map((todo, index) => (
+            <TodoItem key={index} text={todo} /> 
+          ))}
         </ScrollView>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#f5f5f5',
   },
   content: {
     flex: 1,
-    paddingTop: 60
-  }
-})
+    paddingTop: 60,
+  },
+});
 
-export default App
+export default App;
